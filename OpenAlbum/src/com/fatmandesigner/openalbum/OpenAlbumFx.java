271708fe82d3d7319de3a70dcf2cab8c0badb74a
Khanh Hua <giakhanh2487@gmail.com>
2025-03-15 13:55:14 +0100

    Config POM with Assembly

--- OpenAlbum/src/com/fatmandesigner/openalbum/OpenAlbumFx.java
+++ OpenAlbum/src/com/fatmandesigner/openalbum/OpenAlbumFx.java
@@ -1,24 +1,14 @@
 package com.fatmandesigner.openalbum;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.fatmandesigner.openalbum.model.Album;
-import com.fatmandesigner.openalbum.model.AlbumList;
-import com.fatmandesigner.openalbum.model.ImageList;
-import com.fatmandesigner.openalbum.model.Photo;
 import com.fatmandesigner.openalbum.sql.Store;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class OpenAlbumFx extends Application  {
@@ -40,7 +30,6 @@ public class OpenAlbumFx extends Application  {
 		Parent root = loader.load();
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
-
 
 		CtrlAlbumBrowser controller = loader.<CtrlAlbumBrowser>getController();
 
