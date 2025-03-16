8b448d25dd8ca63ed94c225a5ed03e7977f028c7
Khanh Hua <giakhanh2487@gmail.com>
2025-03-16 19:49:34 +0100

    clean up data directory

--- OpenAlbum/run.sh
+++ OpenAlbum/run.sh
@@ -2,6 +2,10 @@
 
 java --module-path /Users/khanhhua/javafx-sdk-23.0.2/lib \
   --add-modules javafx.controls,javafx.fxml \
-  -cp target/OpenAlbum-0.0.1-SNAPSHOT.jar:lib/* \
+  -cp target/OpenAlbum-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
   com.fatmandesigner.openalbum.OpenAlbumFx
 
+#java -cp target/OpenAlbum-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+#  --module-path target \
+#  --add-modules javafx.controls,javafx.fxml \
+#  com.fatmandesigner.openalbum.OpenAlbumFx
